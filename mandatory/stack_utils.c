0952e7d23bd515fd6af63fceb9c106f952ca75dc
Youssef El Aissaoui <90868691+YusufLisawi@users.noreply.github.com>
2023-02-04 10:48:54 +0100

    Pop & push

--- mandatory/stack_utils.c
+++ mandatory/stack_utils.c
@@ -6,7 +6,7 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/03 20:06:40 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/02/03 22:53:04 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/02/04 10:46:33 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ void	push(t_list **stack, int popped)
 	t_list	*node;
 
 	node = ft_lstnew(popped);
-	ft_lstadd_front(stack, node);
+	ft_lstadd_front(*stack, node);
 }
 
-int	pop(t_stack *stack)
+int	pop(t_list **stack)
 {
 	t_list	*tmp;
 	int		popped;
 
-	if (stack->top_a == 0)
+	if (*stack == 0)
 		return (-1);
-	popped = stack->top_a->content;
-	tmp = stack->top_a;
-	stack->top_a = tmp->next;
+	popped = (*stack)->content;
+	tmp = *stack;
+	*stack = tmp->next;
 	free(tmp);
 	return (popped);
 }
