898829aadc8e655bdf46fe1ec0ef11bc705788eb
YusufLisawi <yusufisawi@gmail.com>
2023-02-13 21:06:09 +0100

    Fixing bonus part

--- bonus/utils_bonus.c
+++ bonus/utils_bonus.c
@@ -14,7 +14,13 @@
 
 void	exec_rule(char *rule, t_stacks *s)
 {
-	if (ft_strncmp(rule, "sa", 2) == 0)
+	if (ft_strncmp(rule, "rra", 3) == 0)
+		rrx(A, s, 0);
+	else if (ft_strncmp(rule, "rrb", 3) == 0)
+		rrx(B, s, 0);
+	else if (ft_strncmp(rule, "rrr", 3) == 0)
+		rrx(R, s, 0);
+	else if (ft_strncmp(rule, "sa", 2) == 0)
 		sx(A, s, 0);
 	else if (ft_strncmp(rule, "sb", 2) == 0)
 		sx(B, s, 0);
@@ -26,12 +32,6 @@ void	exec_rule(char *rule, t_stacks *s)
 		rx(B, s, 0);
 	else if (ft_strncmp(rule, "rr", 2) == 0)
 		rx(R, s, 0);
-	else if (ft_strncmp(rule, "rra", 3) == 0)
-		rrx(A, s, 0);
-	else if (ft_strncmp(rule, "rrb", 3) == 0)
-		rrx(B, s, 0);
-	else if (ft_strncmp(rule, "rrr", 3) == 0)
-		rrx(R, s, 0);
 	else if (ft_strncmp(rule, "pa", 2) == 0)
 		px(A, s, 0);
 	else if (ft_strncmp(rule, "pb", 2) == 0)
